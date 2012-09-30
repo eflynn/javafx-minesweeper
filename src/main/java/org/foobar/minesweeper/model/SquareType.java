b0f3914ce633aba7845b28f89da7cdd63d00c583
Evan Flynn <evan.flynn@gmail.com>
2012-09-30 14:18:26 -0400

    eclipse source formatting / unfuck line endings

--- src/main/java/org/foobar/minesweeper/model/SquareType.java
+++ src/main/java/org/foobar/minesweeper/model/SquareType.java
@@ -4,48 +4,47 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 
 public enum SquareType {
-    BLANK, FLAG, MINE, HITMINE, WRONGMINE,
-    ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(4), 
-    FIVE(5), SIX(6), SEVEN(7), EIGHT(8);
+	BLANK, FLAG, MINE, HITMINE, WRONGMINE, ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(
+			4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8);
 
-    private final int mineCount;
+	private final int mineCount;
 
-    private SquareType(int mineCount) {
-        this.mineCount = mineCount;
-    }
+	private SquareType(int mineCount) {
+		this.mineCount = mineCount;
+	}
 
-    private SquareType() {
-        this(-1);
-    }
-    
-    public boolean isRevealable() {
-        return this == BLANK;
-    }
+	private SquareType() {
+		this(-1);
+	}
 
-    public boolean hasMineCount() {
-        return mineCount != -1;
-    }
+	public boolean isRevealable() {
+		return this == BLANK;
+	}
 
-    public int getMineCount() {
-        checkState(hasMineCount());
+	public boolean hasMineCount() {
+		return mineCount != -1;
+	}
 
-        return mineCount;
-    }
+	public int getMineCount() {
+		checkState(hasMineCount());
 
-    /**
-     * Returns default value if not a member.
-     *
-     * @param defaultValue
-     * @return
-     */
-    public int getMineCountOr(int defaultValue) {
-        return hasMineCount() ? mineCount : defaultValue;
-    }
+		return mineCount;
+	}
 
-    public static SquareType of(int numMines) {
-        checkArgument(numMines >= 0 && numMines <= 8, 
-                "numMines (%d) is out of bounds", numMines);
-        
-        return SquareType.values()[ZERO.ordinal() + numMines];
-    }
+	/**
+	 * Returns default value if not a member.
+	 * 
+	 * @param defaultValue
+	 * @return
+	 */
+	public int getMineCountOr(int defaultValue) {
+		return hasMineCount() ? mineCount : defaultValue;
+	}
+
+	public static SquareType of(int numMines) {
+		checkArgument(numMines >= 0 && numMines <= 8,
+				"numMines (%d) is out of bounds", numMines);
+
+		return SquareType.values()[ZERO.ordinal() + numMines];
+	}
 }
