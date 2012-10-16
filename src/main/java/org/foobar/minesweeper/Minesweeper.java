1ec7cf29cd696a6e6be0e68deb15b2a398f8e4ab
Evan Flynn <evan.flynn@gmail.com>
2012-10-16 14:36:20 -0400

    update license information

--- src/main/java/org/foobar/minesweeper/Minesweeper.java
+++ src/main/java/org/foobar/minesweeper/Minesweeper.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2012 Evan Flynn
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package org.foobar.minesweeper;
 
 import com.google.common.eventbus.EventBus;
@@ -10,21 +29,19 @@ import org.foobar.minesweeper.model.Minefield;
 public class Minesweeper extends Application {
   private final EventBus eventBus = new EventBus();
   private final Minefield field = new Minefield(eventBus);
-  
-  public Minesweeper() {
-  }
+
+  public Minesweeper() {}
 
   @Override
   public void start(Stage stage) {
     Parent root = new MinesweeperPane(field, eventBus);
     eventBus.register(root);
-    
+
     stage.setResizable(false);
     stage.setScene(new Scene(root, 260, 300));
     stage.setTitle("Minesweeper");
     stage.show();
   }
-
 
   public static void main(String[] args) {
     launch(args);
