f38dc844b544b14306f2f7cab66c5247e8f4318f
Evan Flynn <evan.flynn@gmail.com>
2012-10-23 15:33:38 -0400

    fix subtle bugs.

--- src/main/java/org/foobar/minesweeper/model/Minefield.java
+++ src/main/java/org/foobar/minesweeper/model/Minefield.java
@@ -294,9 +294,9 @@ public final class Minefield {
 
     List<Entry> neighbors = new ArrayList<Entry>(8);
 
-    for(int r = row - 1; r < row + 1; r++) {
-      for(int c = column - 1; c < column + 1; c++) {
-        if (r != row && c != column && r >= 0 && c >= 0 && r < rows && c < columns)
+    for(int r = row - 1; r <= row + 1; r++) {
+      for(int c = column - 1; c <= column + 1; c++) {
+        if ((r != row || c != column) && r >= 0 && c >= 0 && r < rows && c < columns)
           neighbors.add(table[r][c]);
       }
     }
