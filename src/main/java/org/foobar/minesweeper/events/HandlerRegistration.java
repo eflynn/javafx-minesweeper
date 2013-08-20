a85dff1178cfb904926c681e67bf383ca70c1a2c
Evan Flynn <evan.flynn@gmail.com>
2013-08-20 18:23:04 -0400

    Start adding event stuff

--- src/main/java/org/foobar/minesweeper/events/HandlerRegistration.java
+++ src/main/java/org/foobar/minesweeper/events/HandlerRegistration.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.foobar.minesweeper.model;
+package org.foobar.minesweeper.events;
 
 /**
  * Use the handler registration to remove handlers when they are no longer needed.
